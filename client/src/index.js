2b9db2d1692a511efa5dba3dae17405f53b01dd5
Iain Stewart <iain.stewart08@gmail.com>
2016-07-29 15:59:02 +0100

    finished basic api searches

--- client/src/index.js
+++ client/src/index.js
@@ -21,9 +21,7 @@ var sendOriginRequest = function() {
 
   req_origin.onload = function() {
     var res_origin = JSON.parse(req_origin.responseText);
-    console.log(res_origin);
     ss_origin = res_origin.Places[0].CityId.substring(0, 3);
-    console.log(ss_origin);
     sendDestinationRequest();
   }
 }
@@ -37,7 +35,6 @@ var sendDestinationRequest = function() {
 
   req_destination.onload = function(){
     var res_destination = JSON.parse(req_destination.responseText);
-    console.log(res_destination);
     ss_destination = res_destination.Places[0].CityId.substring(0, 3)
     console.log(ss_destination);
     sendSearchRequests();
@@ -47,11 +44,10 @@ var sendDestinationRequest = function() {
 var sendSearchRequests = function() {
   var url_ss = "http://partners.api.skyscanner.net/apiservices/browsedates/v1.0/GB/GBP/en-GB/" + ss_origin + "/" + ss_destination + "/" + startDate + "/" + endDate + "?apiKey=co666659065635714271429118382522";
 
-  var url_exp = "http://terminal2.expedia.com/x/mhotels/search?city=berlin&checkInDate=2016-12-15&checkOutDate=2016-12-17&sortOrder=true&room1=2&apikey=fZPSPARW8ZW6Yg738AzbASiN8VPFwVos";
+  var url_exp = "http://terminal2.expedia.com/x/mhotels/search?city=" + origin + "&checkInDate=" + startDate + "&checkOutDate=" + endDate + "&room1=" + noRoomsValue + "&resultsPerPage=-1&apikey=fZPSPARW8ZW6Yg738AzbASiN8VPFwVos";
 
   var req_ss = new XMLHttpRequest();
   req_ss.open("GET", url_ss);
-  // request.setRequestHeader('accept', 'application/json');
   req_ss.send(null);
   req_ss.onload = function(){
     var res_ss = JSON.parse(req_ss.responseText);
@@ -60,11 +56,9 @@ var sendSearchRequests = function() {
 
   var req_exp = new XMLHttpRequest();
   req_exp.open("GET", url_exp);
-  // request.setRequestHeader('accept', 'application/json');
   req_exp.send(null);
   req_exp.onload = function(){
     var res_exp = JSON.parse(req_exp.responseText);
     console.log(res_exp);
-    console.log(res_exp.hotelList[0].lowRateInfo.total);
   }
 }
