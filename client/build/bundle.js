bb88813d45af6770d8dd8beaa5ad8bdfde3a40f9
Iain Stewart <iain.stewart08@gmail.com>
2016-07-31 17:03:06 +0100

    flight data now displaying

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -104,7 +104,7 @@
 	  req_ss.open("GET", url_ss);
 	  req_ss.send(null);
 	  req_ss.onload = function(){
-	    var res_ss = JSON.parse(req_ss.responseText);
+	    res_ss = JSON.parse(req_ss.responseText);
 	    console.log(res_ss);
 	  }
 	
@@ -112,25 +112,72 @@
 	  req_exp.open("GET", url_exp);
 	  req_exp.send(null);
 	  req_exp.onload = function(){
-	    var res_exp = JSON.parse(req_exp.responseText);
+	    res_exp = JSON.parse(req_exp.responseText);
 	    console.log(res_exp);
 	    displayFlightResults();
 	    displayHotelResults();
 	  }
 	}
 	
+	
+	
+	
+	
 	var displayFlightResults = function() {
-	  var flightResult1 = document.getElementById("flight1");
-	  var flightResult2 = document.getElementById("flight2");
-	  var flightResult3 = document.getElementById("flight3");
-	  var flightResult4 = document.getElementById("flight4");
-	  var flightResult5 = document.getElementById("flight5");
+	  var displayFlightsArray = createDisplayHandles();
+	
+	  for (var i = 0; i < displayFlightsArray.length; i++) {
+	    if (res_ss.Quotes[i]) { // if there is a valid quote then print it
+	      // but only if the trip has an inbound and outbound journey
+	      if (res_ss.Quotes[i].InboundLeg && res_ss.Quotes[i].OutboundLeg) {
+	        var airlineID = res_ss.Quotes[i].InboundLeg.CarrierIds[0];
+	        var airlineName = getAirlineName(airlineID);
+	
+	        var flightResultDetails = {"price": res_ss.Quotes[i].MinPrice, "direct": res_ss.Quotes[i].Direct, "outboundDate": res_ss.Dates.OutboundDates[0].PartialDate, "inboundDate": res_ss.Dates.InboundDates[0].PartialDate, "airline": airlineName}
+	        addResultToPage(flightResultDetails, displayFlightsArray, i);
+	      } else {
+	        continue;
+	      }
+	      }
+	  }
+	}
+	
+	// gets a handle on the divs used to display the flight results
+	var createDisplayHandles = function() {
+	  var resultsArray = [];
+	  resultsArray[0] = document.getElementById("flight-result1");
+	  resultsArray[1] = document.getElementById("flight-result2");
+	  resultsArray[2] = document.getElementById("flight-result3");
+	  resultsArray[3] = document.getElementById("flight-result4");
+	  resultsArray[4] = document.getElementById("flight-result5");
+	  return resultsArray;
+	}
+	
+	// returns the airline name, given the airline ID
+	var getAirlineName = function(airlineID) {
+	  var airlinesArray = res_ss.Carriers;
+	  for (var i in airlinesArray) {
+	    if (airlineID === airlinesArray[i].CarrierId) {
+	      return airlinesArray[i].Name;
+	    }
+	  }
+	}
+	
+	var addResultToPage = function(flight, results, index) {
+	  results[index].innerHTML += "<p><b>" + flight.airline + "</b></p>";
+	  results[index].innerHTML += "<p>" + "£" + flight.price + "</p>";
+	  if (flight.direct) {
+	    results[index].innerHTML += "<p>" + "Direct flight" + "</p>";
+	  } else {
+	    results[index].innerHTML += "<p>" + "Indirect flight" + "</p>";
+	  }
+	  results[index].innerHTML += "<p>" + "Departure date: " + flight.outboundDate + "</p>";
+	  results[index].innerHTML += "<p>" + "Return date: " + flight.inboundDate + "</p>";
 	}
 	
 	var displayHotelResults = function() {
-	  
+	
 	}
-
 
 /***/ },
 /* 1 */
